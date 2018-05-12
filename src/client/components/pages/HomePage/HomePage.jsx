98103b5bee523d086e00b1485f35136eeab3021c
auroratide <tf.auroratide@gmail.com>
2018-05-12 18:52:13 -0500

    [#5] Add digests link to the front page

--- src/client/components/pages/HomePage/HomePage.jsx
+++ src/client/components/pages/HomePage/HomePage.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import Link from 'Client/components/core/Link';
 
 const HomePage = () =>
   <div>
     <p>Hello World!</p>
+    <p><Link to='/digests'>Go to Digests</Link></p>
   </div>;
 
 export default HomePage;
