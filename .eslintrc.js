155cbdb792f21eacd2724562d8ab4b2ede577f84
auroratide <tf.auroratide@gmail.com>
2018-05-12 17:48:20 -0500

    [#15] Added linting

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true
+  },
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  parserOptions: {
+    ecmaFeatures: {
+      experimentalObjectRestSpread: true,
+      jsx: true
+    },
+    sourceType: 'module'
+  },
+  plugins: ['react'],
+  rules: {
+    'indent': ['error', 2, { SwitchCase: 1 }],
+    'linebreak-style': ['error', 'unix'],
+    'quotes': ['error', 'single'],
+    'semi': ['error', 'always'],
+    'no-console': 'warn',
+    'dot-notation': 'error',
+    'eqeqeq': ['error', 'always'],
+    'no-eval': 'error',
+    'no-path-concat': 'error',
+    'brace-style': 'error',
+    'comma-dangle': ['error', 'never'],
+    'func-style': ['error', 'expression'],
+    'no-var': 'error',
+    'object-shorthand': 'error',
+    'prefer-arrow-callback': 'error',
+    'prefer-const': 'error',
+    'react/prefer-es6-class': 'error',
+    'react/prefer-stateless-function': 'error',
+  }
+};
