b598e513cc3fdd19cb9d68188f07fdd9dff7efe0
auroratide <tf.auroratide@gmail.com>
2018-05-19 11:16:01 -0500

    [#19] First pass at a togglable top bar

--- src/client/components/layout/TopBar/TopBar.jsx
+++ src/client/components/layout/TopBar/TopBar.jsx
@@ -2,16 +2,24 @@ import React from 'react';
 import Container from 'Client/components/core/Container';
 import LogoLink from './LogoLink';
 import NavLink from './NavLink';
+import Accordion from 'Client/components/core/Accordion';
 
 import styles from './style';
+
+const state = new Accordion.State();
 
 const TopBar = () =>
   <nav className={styles['top-bar']}>
     <Container className={styles.container}>
-      <LogoLink />
-      <div className={styles['nav-links']}>
-        <NavLink name='Digests' />
+      <div className={styles['main-links']}>
+        <LogoLink />
+        <button className={styles.hamburger} onClick={state.toggle}>T</button>
       </div>
+      <Accordion state={state} className={styles['nav-links']}>
+        <NavLink name='Posts' />
+        <NavLink name='Digests' />
+        <NavLink name='Stories' />
+      </Accordion>
     </Container>
   </nav>;
 
