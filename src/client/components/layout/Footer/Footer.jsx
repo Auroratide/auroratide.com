a137c175c3d51d005aadfaba93d4cb199d968d26
auroratide <tf.auroratide@gmail.com>
2018-05-20 07:32:30 -0500

    [#20] Added footer component

---
+++ src/client/components/layout/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Container from 'Client/components/core/Container';
+
+import './style';
+
+const Footer = () =>
+  <footer>
+    <Container>
+      <p>This is a footer.</p>
+    </Container>
+  </footer>;
+
+export default Footer;
--- src/client/components/layout/Page/Page.jsx
+++ src/client/components/layout/Page/Page.jsx
@@ -3,11 +3,13 @@ import PropTypes from 'prop-types';
 
 import Header from 'Client/components/layout/Header';
 import Main from 'Client/components/layout/Main';
+import Footer from 'Client/components/layout/Footer';
 
 const Page = ({ children }) =>
   <div>
     <Header />
     <Main>{children}</Main>
+    <Footer />
   </div>;
 
 Page.propTypes = {
