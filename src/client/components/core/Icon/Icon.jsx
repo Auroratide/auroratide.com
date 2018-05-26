3249018b71ab3d1f74e0a047abd9c5d04b3f0714
auroratide <tf.auroratide@gmail.com>
2018-05-26 18:18:40 -0500

    [#43] Added key icon for the current digest

--- src/client/components/core/Icon/Icon.jsx
+++ src/client/components/core/Icon/Icon.jsx
@@ -2,20 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import classnames from 'classnames';
-
-import { faBars } from '@fortawesome/free-solid-svg-icons';
-import {
-  faTwitter,
-  faLinkedinIn,
-  faGithubAlt
-} from '@fortawesome/free-brands-svg-icons';
-
-const library = {
-  'bars': faBars,
-  'twitter': faTwitter,
-  'linked-in': faLinkedinIn,
-  'github-alt': faGithubAlt
-};
+import library from './library';
 
 const Icon = ({ icon, className }) =>
   <FontAwesomeIcon icon={library[icon]} className={classnames(className)} />;
---
+++ src/client/components/core/Icon/library.js
@@ -0,0 +1,17 @@
+import {
+  faBars,
+  faKey
+} from '@fortawesome/free-solid-svg-icons';
+import {
+  faTwitter,
+  faLinkedinIn,
+  faGithubAlt
+} from '@fortawesome/free-brands-svg-icons';
+
+export default {
+  'bars': faBars,
+  'key': faKey,
+  'twitter': faTwitter,
+  'linked-in': faLinkedinIn,
+  'github-alt': faGithubAlt
+};
