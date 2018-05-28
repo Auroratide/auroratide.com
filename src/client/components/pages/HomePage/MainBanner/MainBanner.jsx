204f177ed600bfa1366bbcf10dbacb7eb894c559
auroratide <tf.auroratide@gmail.com>
2018-05-28 12:52:30 -0500

    [#50] Desktop styling for main banner

--- src/client/components/pages/HomePage/MainBanner/MainBanner.jsx
+++ src/client/components/pages/HomePage/MainBanner/MainBanner.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
+import Container from 'Client/components/core/Container';
 import Logo from 'Client/components/core/Logo';
 import styles from './style';
 
 const MainBanner = () =>
   <section className={styles['main-banner']}>
-    <Logo className={styles.logo} />
-    <div className={styles.text}>
-      <h1>Auroratide</h1>
-      <div className={styles.subtitle}>
-        <span>Coder</span><span>Writer</span><span>Dreamer</span>
+    <Container className={styles.container}>
+      <Logo className={styles.logo} />
+      <div className={styles.text}>
+        <h1>Auroratide</h1>
+        <div className={styles.subtitle}>
+          <span>Coder</span><span>Writer</span><span>Dreamer</span>
+        </div>
       </div>
-    </div>
+    </Container>
   </section>;
 
 export default MainBanner;
