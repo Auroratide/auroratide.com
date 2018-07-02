c4570643d63cee4bb0ad036197c9b63fc743c6a3
auroratide <tf.auroratide@gmail.com>
2018-07-02 16:52:38 -0500

    [#53] Hide away magic constants behind a custom selector

--- postcss.config.js
+++ postcss.config.js
@@ -3,7 +3,10 @@ const path = require('path');
 module.exports = {
   plugins: [
     require('postcss-import')({
-      path: [path.resolve(__dirname, 'src', 'client', 'styles')]
+      path: [
+        path.resolve(__dirname, 'src', 'client', 'styles'),
+        path.resolve(__dirname, 'src', 'client', 'components')
+      ]
     }),
     require('postcss-cssnext')
   ]
