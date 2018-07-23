f4711fb235653229b54fb3750b7b3c1e5b6a3e82
auroratide <tf.auroratide@gmail.com>
2018-07-22 19:25:13 -0500

    [#53] Animate the dots image

--- src/client/components/pages/HomePage/AboutMeSection/DotsImage/DotsImage.jsx
+++ src/client/components/pages/HomePage/AboutMeSection/DotsImage/DotsImage.jsx
@@ -8,9 +8,9 @@ import styles from './style';
 const DotsImage = ({ className }) =>
   <Animated.figure className={classnames(styles['dots-image'], className)}>
     <img src={Assets.Images.DOTS_BASE} className={styles.base} />
-    <img src={Assets.Images.DOTS_LINE_TOP} className={styles.top} />
-    <img src={Assets.Images.DOTS_LINE_MIDDLE} className={styles.middle} />
-    <img src={Assets.Images.DOTS_LINE_BOTTOM} className={styles.bottom} />
+    <img src={Assets.Images.DOTS_LINE_TOP} className={classnames(styles.top, styles.animated)} />
+    <img src={Assets.Images.DOTS_LINE_MIDDLE} className={classnames(styles.middle, styles.animated)} />
+    <img src={Assets.Images.DOTS_LINE_BOTTOM} className={classnames(styles.bottom, styles.animated)} />
   </Animated.figure>;
 
 DotsImage.propTypes = {
