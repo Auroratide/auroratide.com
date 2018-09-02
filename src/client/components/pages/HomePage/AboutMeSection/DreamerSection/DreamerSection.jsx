f5a423c6bd7646628dad7e4fe9707e66d557dc79
auroratide <tf.auroratide@gmail.com>
2018-09-02 16:54:01 -0500

    [#53] Added dreamer text for now

--- src/client/components/pages/HomePage/AboutMeSection/DreamerSection/DreamerSection.jsx
+++ src/client/components/pages/HomePage/AboutMeSection/DreamerSection/DreamerSection.jsx
@@ -4,9 +4,9 @@ import Subsection from '../Subsection';
 const DreamerSection = () =>
   <Subsection image={() => null} direction={Subsection.Direction.TOP_TO_BOTTOM}>
     <h2>I Dream</h2>
-    <p>Something Something text</p>
-    <p>Something something text</p>
-    <p>More Text</p>
+    <p>Stories fascinate me. They surround us in the forms of books, games, movies, anecdotes. Yet, many stories are fictional, completely invented by the imaginative wanderings of a creative person.</p>
+    <p>I have had many such wanderings, creating colorful characters, envisioning their worlds, and forming the events that shape their lives.</p>
+    <p>Whether it be about a world where magic is coded or about an unassuming girl accidentally uncovering a nationwide conspiracy, there&apos;s always a story to tell. My aspiration is to put many of these dreams to paper.</p>
   </Subsection>;
 
 export default DreamerSection;
