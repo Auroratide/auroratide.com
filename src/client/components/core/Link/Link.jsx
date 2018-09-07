81aa11da54ddb8d97eace5c893e7787baa6dee26
auroratide <tf.auroratide@gmail.com>
2018-09-07 07:42:59 -0500

    Refactor Link to use renderIf

--- src/client/components/core/Link/Link.jsx
+++ src/client/components/core/Link/Link.jsx
@@ -1,24 +1,17 @@
 import React from 'react';
 import PropTypes from 'Client/utils/prop-types';
 import { Link as RouterLink } from 'react-router-dom';
+import { renderIfElse } from 'Client/utils/render-if';
 import classnames from 'classnames';
 
 const isExternal = link => /http/.test(link);
 
-const renderInternalLink = ( { to, className, onClick, children } ) =>  // eslint-disable-line react/prop-types
-  <RouterLink to={to} onClick={onClick} className={classnames(className)}>{children}</RouterLink>;
-
-const renderExternalLink = ( { to, newTab, className, children } ) =>  // eslint-disable-line react/prop-types
-  <a href={to} target={newTab ? '_blank' : undefined} className={classnames(className)}>{children}</a>;
-
-const Link = (props) => {
-  const to = props.to;
-  if(isExternal(to)) {
-    return renderExternalLink(props);
-  } else {
-    return renderInternalLink(props);
-  }
-};
+const Link = ({ to, newTab, onClick, className, children }) =>
+  renderIfElse(isExternal(to), () =>
+    <a href={to} target={newTab ? '_blank' : undefined} className={classnames(className)}>{children}</a>
+  ).elseRender(() =>
+    <RouterLink to={to} onClick={onClick} className={classnames(className)}>{children}</RouterLink>
+  );
 
 Link.propTypes = {
   to: PropTypes.string.isRequired,
