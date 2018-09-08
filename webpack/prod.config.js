a4a3f4f1562801e072d67ecf3258e5b1f1737ffe
auroratide <tf.auroratide@gmail.com>
2018-09-07 20:20:46 -0500

    [#76] Optimize CSS

--- webpack/prod.config.js
+++ webpack/prod.config.js
@@ -1,6 +1,10 @@
 const merge = require('webpack-merge');
 const base = require('./base.config');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 module.exports = merge(base, {
-  mode: 'production'
+  mode: 'production',
+  plugins: [
+    new OptimizeCssAssetsPlugin({})
+  ]
 });
