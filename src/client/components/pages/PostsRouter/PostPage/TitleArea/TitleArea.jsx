657bca4fa85e5cfca635330315de38635562f880
auroratide <tf.auroratide@gmail.com>
2018-09-22 13:59:36 +0800

    [#80] Eliminate warning about icon not being renderable when icon is missing

--- src/client/components/pages/PostsRouter/PostPage/TitleArea/TitleArea.jsx
+++ src/client/components/pages/PostsRouter/PostPage/TitleArea/TitleArea.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import PropTypes from 'Client/utils/prop-types';
 import Icon from 'Client/components/core/Icon';
 import classnames from 'classnames';
+import { renderIf } from 'Client/utils/render-if';
 
 import styles from './style';
 
 const TitleArea = ({ title, color, icon }) =>
   <header className={classnames(styles['title-area'], styles[`color-${color}`])}>
-    <Icon icon={icon} className={styles.icon} />
+    {renderIf(icon, () => <Icon icon={icon} className={styles.icon} />)}
     <h1 className={styles.title}>{title}</h1>
   </header>;
 
