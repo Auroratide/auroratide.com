a065192be1564e2bc30900abf6826cffcd51a8bc
auroratide <tf.auroratide@gmail.com>
2019-02-04 21:07:05 +0530

    [#116] Center license

--- src/client/components/layout/Footer/License/License.jsx
+++ src/client/components/layout/Footer/License/License.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { currentYear } from 'Client/utils/date';
+import styles from './style';
 
-const License = () => <div className='license-wrap'>
-  <p className='license'>&copy; {currentYear()} Timothy Foster, All Rights Reserved</p>
-</div>;
+const License = () =>
+  <p className={styles.license}>&copy; {currentYear()} Timothy Foster, All Rights Reserved</p>;
 
 export default License;
