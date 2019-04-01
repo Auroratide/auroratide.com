020fc4a9c19f77000c1b8f049d6318bca6d3e6b8
auroratide <tf.auroratide@gmail.com>
2019-04-01 17:18:58 +0800

    [#137] Add images of team members to the whodoku

--- src/client/components/widgets/Whodoku/Square/Square.jsx
+++ src/client/components/widgets/Whodoku/Square/Square.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { renderIf } from 'Client/utils/render-if';
 import styles from './style';
 
 const Square = ({ value, onClick }) =>
-  <div className={styles.square} onClick={onClick}>{value}</div>;
+  <div className={styles.square} onClick={onClick}>
+    {renderIf(value, () => <img src={`/assets/whodoku/${value}.png`} alt={value} />)}
+  </div>;
 
 Square.propTypes = {
   value: PropTypes.number,
