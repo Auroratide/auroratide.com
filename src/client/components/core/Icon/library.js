4a7a9481db7a00e22aab706f988b9b4dfd963b7a
auroratide <tf.auroratide@gmail.com>
2019-05-02 07:32:45 -0500

    [D] test-commit-revert

--- src/client/components/core/Icon/library.js
+++ src/client/components/core/Icon/library.js
@@ -28,6 +28,7 @@ import { faLayerGroup } from '@fortawesome/free-solid-svg-icons/faLayerGroup';
 import { faCube } from '@fortawesome/free-solid-svg-icons/faCube';
 import { faUserCog } from '@fortawesome/free-solid-svg-icons/faUserCog';
 import { faCode } from '@fortawesome/free-solid-svg-icons/faCode';
+import { faCheckDouble } from '@fortawesome/free-solid-svg-icons/faCheckDouble';
 
 import { faTwitter } from '@fortawesome/free-brands-svg-icons/faTwitter';
 import { faLinkedinIn } from '@fortawesome/free-brands-svg-icons/faLinkedinIn';
@@ -68,6 +69,7 @@ export default {
   'cube': faCube,
   'user-cog': faUserCog,
   'code': faCode,
+  'check-double': faCheckDouble,
   'twitter': faTwitter,
   'linked-in': faLinkedinIn,
   'github-alt': faGithubAlt,
