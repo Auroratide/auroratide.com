f97a3f464d6186d027098a9aa5d075ecf08ed075
auroratide <tf.auroratide@gmail.com>
2019-06-03 16:47:19 -0500

    Add link to post on creativity so people know what the heck that picture is on the front page

--- src/client/components/pages/HomePage/AboutMeSection/TeacherSection/TeacherSection.jsx
+++ src/client/components/pages/HomePage/AboutMeSection/TeacherSection/TeacherSection.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
+import Muted from 'Client/components/core/Muted';
+import Link from 'Client/components/core/Link';
 import Subsection from '../Subsection';
 import DotsImage from '../DotsImage';
+import { UrlBuilder } from 'Client/config/links';
 
 const TeacherSection = () =>
   <Subsection image={DotsImage} direction={Subsection.Direction.RIGHT_TO_LEFT}>
@@ -9,6 +12,7 @@ const TeacherSection = () =>
     <p>See, teaching is not about stuffing information into people&apos;s heads. Teaching is about cultivating curiosity.</p>
     <p>This website is one of my ways of sharing ideas and experiences in order to do just that. Though I want people leave my site knowing a little more about coding and storytelling, more importantly I hope people leave asking deeper questions or experimenting on their own.</p>
     <p>And hopefully that&apos;s reflected in my writing style!</p>
+    <Muted.small>That funky picture on the left demonstrates an important concept in creativity. <Link to={new UrlBuilder().post('connecting-dots-and-unassuming-assumptions')}>Read what I wrote</Link>!</Muted.small>
   </Subsection>;
 
 export default TeacherSection;
