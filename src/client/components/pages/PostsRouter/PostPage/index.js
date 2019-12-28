4859b4d8929cd9547f31592da2d10e593510aecd
auroratide <tf.auroratide@gmail.com>
2019-12-28 13:42:02 -0600

    Update dependencies

--- src/client/components/pages/PostsRouter/PostPage/index.js
+++ src/client/components/pages/PostsRouter/PostPage/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'Client/utils/prop-types';
 import useAsync from '@auroratide/use-async';
 import withId from './with-id';
 import PostPage from './PostPage';
@@ -9,6 +10,12 @@ import withResourceItem from 'Client/components/core/with-resource-item';
 const WithListLoading = props => {
   useAsync(props.resource.refreshList).andCall();
   return <PostPage {...props} />;
+};
+
+WithListLoading.propTypes = {
+  resource: PropTypes.shape({
+    refreshList: PropTypes.func
+  })
 };
 
 export default withId(withResourceItem(PostsContext)(WithListLoading));
