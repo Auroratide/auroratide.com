446ca9f9f31f17ce4686507d650d03ebb9313cde
BuildTools <unconfigured@null.spigotmc.org>
2021-01-05 09:57:30 -0600

    Configure live reloading

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,8 @@ import svelte from 'rollup-plugin-svelte'
 import css from 'rollup-plugin-css-only'
 import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
+import livereload from 'rollup-plugin-livereload'
+import { terser } from 'rollup-plugin-terser'
 
 const production = !process.env.ROLLUP_WATCH
 const clientSrc = path.resolve(__dirname, 'src', 'client')
@@ -17,22 +19,30 @@ export default {
         file: path.join(clientOut, 'bundle.js'),
     },
     plugins: [
+        // Compile svelte program
         svelte({
             compilerOptions: {
                 dev: !production
             }
         }),
         
+        // Collect CSS into a file, for browser performance
         css({
             output: 'bundle.css'
         }),
 
+        // Resolve dependencies
         resolve({
             browser: true,
             dedupe: ['svelte'],
         }),
-
         commonjs(),
+
+        // Refresh browser when changes occur in public
+        !production && livereload('public'),
+
+        // Minify for production
+        production && terser()
     ],
     watch: {
         clearScreen: false
