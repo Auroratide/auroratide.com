fba9df25f37f147e4d51a822a3d485c371508a59
Timothy Foster <tf.auroratide@gmail.com>
2021-01-16 17:12:19 -0600

    Add content component

---
+++ src/client/Content/Content.svelte
@@ -0,0 +1,27 @@
+<div class="content" class:has-sidebar={$$slots.sidebar}>
+    <slot></slot>
+    <slot name="sidebar"></slot>
+</div>
+
+<style>
+    .content {
+        background: var(--skin-color-content);
+        padding: var(--sizing-spacing-md);
+    }
+
+    @media screen and (min-width: 75rem) {
+        .content {
+            padding: var(--sizing-spacing-xl);
+        }
+
+        .content.has-sidebar {
+            display: grid;
+            grid-template-columns: 3fr 1fr;
+            padding: var(--sizing-spacing-xl) calc(var(--sizing-spacing-xl) - var(--sizing-spacing-sm));
+        }
+
+        .content.has-sidebar > * {
+            padding: 0 var(--sizing-spacing-sm);
+        }
+    }
+</style>
--- src/client/posts/PostPage/PostPage.svelte
+++ src/client/posts/PostPage/PostPage.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
     import { Container } from '@/client/Container'
+    import { Content } from '@/client/Content'
     import { Loading } from '@/client/Loading'
 
     import type { PostsApi } from '../api'
@@ -19,41 +20,26 @@
     {:then item}
         <article style={`--article-color: var(--palette-${item.color});`}>
             <Header title={item.title} icon={item.icon} />
-            <div class="body">
+            <Content>
                 <section class="content">
                     <div class="published"><DateDisplay date={item.publishedAt} /></div>
                     {@html item.content}
                 </section>
-                <aside>
+                <aside slot="sidebar">
                     <h2 class="more-title">More on {item.category}</h2>
                 </aside>
-            </div>
+            </Content>
         </article>
     {/await}
 </Container>
 
 <style>
-    .body {
-        background: var(--skin-color-content);
-        padding: var(--sizing-spacing-md);
-    }
-
     .published {
         margin-bottom: var(--sizing-spacing-p);
         font-weight: var(--typography-light);
     }
 
     @media screen and (min-width: 75rem) {
-        .body {
-            display: grid;
-            grid-template-columns: 3fr 1fr;
-            padding: var(--sizing-spacing-xl) calc(var(--sizing-spacing-xl) - var(--sizing-spacing-sm));
-        }
-
-        .body > * {
-            padding: 0 var(--sizing-spacing-sm);
-        }
-
         .more-title {
             font-weight: var(--typography-light);
             font-size: var(--sizing-font-sm);
