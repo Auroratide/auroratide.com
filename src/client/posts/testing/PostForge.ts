7e1c6c54005615c5d85318d064e53013e7be20bf
Timothy Foster <tf.auroratide@gmail.com>
2021-02-16 09:50:46 -0600

    Refine App Test into an end to end test, sort of

--- src/client/posts/testing/PostForge.ts
+++ src/client/posts/testing/PostForge.ts
@@ -1,21 +1,22 @@
 import type { Post } from '../types'
 
 type Options = {
-    title?: string
+    title?: string,
+    content?: string,
 }
 
 export class PostForge {
     create(id: string, options: Options = {}): Post {
         return {
             id,
-            title: options.title ? options.title : id,
+            title: options.title ?? id,
             category: 'Fruit',
             icon: 'apple',
             color: 'red',
             summary: 'A summary',
             publishedAt: new Date('2021-01-01T00:00:00.000Z'),
             createdAt: new Date('2021-01-01T00:00:00.000Z'),
-            content: '<p>Content</p>',
+            content: options.content ?? '<p>Content</p>',
         }
     }
 }
