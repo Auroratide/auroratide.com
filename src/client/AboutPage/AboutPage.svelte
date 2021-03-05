a990a86885594b0218910d0300bfb9bed15890ca
Timothy Foster <tf.auroratide@gmail.com>
2021-03-05 08:09:11 -0600

    Temporary about page

--- src/client/AboutPage/AboutPage.svelte
+++ src/client/AboutPage/AboutPage.svelte
@@ -1,11 +1,15 @@
 <script lang="ts">
     import { DocumentInfo } from '@/client/DocumentInfo'
     import { Container } from '@/client/Container'
+    import { Content } from '@/client/Content'
 </script>
 
 
 <DocumentInfo title="About" description="My name's Timothy! I code and teach for a living, and sometimes I write about programming, worldbuilding, and stuff I've built.">
     <Container>
-        <p>About</p>
+        <Content>
+            <h1>About Me</h1>
+            <p>Coming soon!</p>
+        </Content>
     </Container>
 </DocumentInfo>
