4ebda5f4dd31c4b891c72d20aa8ecc45fcbc35e0
Timothy Foster <tf.auroratide@gmail.com>
2021-04-16 16:28:28 -0500

    Update about page

--- src/client/AboutPage/AboutPage.svelte
+++ src/client/AboutPage/AboutPage.svelte
@@ -14,7 +14,7 @@
                 <p>A long time ago, I had a graphing calculator which came with a bunch of games on it. But one day, a nefarious colleague deleted all those games. Desperate, I sought a manual hoping they could be restored, but I stumbled upon something much better.</p>
                 <p>The games could not be undeleted. Instead, they could be recreated.</p>
                 <p>That's how I discovered programming. Since then, I've dabbled in websites, game development, artificial intelligence, and more. It turns out coding is much like magic.</p>
-                <p>Nowadays, I work for a brilliant software company called <a href="https://www.thoughtworks.com">ThoughtWorks</a>. I deal mostly with Javascript code (fun stuff like Svelte, React, or Pixi). So if you're a frontend developer or perhaps a game designer, hopefully you will find the content on my site enriching!</p>
+                <p>Nowadays, I work as an independent developer. I deal mostly with Javascript code (fun stuff like Svelte, React, or Pixi). So if you're a frontend developer or perhaps a game designer, hopefully you will find the content on my site enriching!</p>
             </Content>
         </article>
     </Container>
