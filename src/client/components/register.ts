4678caa5a76d532079762a442ffec578085dd576
Timothy Foster <tf.auroratide@gmail.com>
2021-04-21 22:30:30 -0500

    [#206] Remove unused manual web component connect

--- src/client/components/register.ts
+++
@@ -1,5 +0,0 @@
-import connect from './connect'
-
-export const register = () => {
-
-}
--- src/client/main.ts
+++ src/client/main.ts
@@ -1,9 +1,7 @@
 import App from './App.svelte'
 import { posts } from './posts/store'
 import { portfolio } from './portfolio/store'
-import { register } from './components/register'
 
-register()
 const app = new App({
     target: document.body,
     props: {
