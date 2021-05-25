47c0b1f4882fb7142e47c23508fda4deb629c51e
Timothy Foster <tf.auroratide@gmail.com>
2021-05-25 09:48:51 -0500

    [#226] Apply custom color to Noon art

--- src/client/art/types.ts
+++ src/client/art/types.ts
@@ -1,10 +1,12 @@
+import type { Color } from '@/client/color'
+
 export type ArtItem = {
     readonly id: string
     readonly title: string
     readonly category: string
     readonly tags: string[]
     readonly image: string
-    readonly color: string
+    readonly color: Color
     readonly summary: string
     readonly publishedAt?: Date
     readonly createdAt: Date
