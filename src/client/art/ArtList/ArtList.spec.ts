0b78ee56b1a6377e663af1b14851f7fe439653ac
Timothy Foster <tf.auroratide@gmail.com>
2021-05-27 09:20:02 -0500

    [#230] List titles for the art pieces

--- src/client/art/ArtList/ArtList.spec.ts
+++ src/client/art/ArtList/ArtList.spec.ts
@@ -1,10 +1,41 @@
 import { ArtList } from '.'
+import { InMemoryResource } from '@/client/resources'
 import { component } from '@/testing/component'
+import { ArtForge } from '../testing/ArtForge'
+import { screen } from '@testing-library/svelte'
+import type { ArtItem } from '../types'
 
 describe('ArtList', () => {
-    test('rendering', () => {
-        expect(() => component(ArtList)
+    let forge: ArtForge
+    let posts: Record<string, ArtItem>
+    let resource: InMemoryResource<ArtItem>
+
+    beforeEach(() => {
+        forge = new ArtForge()
+    })
+
+    test('items not loaded yet', () => {
+        resource = new InMemoryResource(null)
+
+        component(ArtList)
+            .prop('resource', resource)
             .render()
-        ).not.toThrow()
+
+        expect(screen.getByText(/fetching/i)).toBeInTheDocument()
+    })
+
+    test('items now loaded', () => {
+        posts = {
+            apple: forge.create('apple', { title: 'Apple' }),
+            orange: forge.create('orange', { title: 'Orange' }),
+        }
+        resource = new InMemoryResource(Object.values(posts))
+
+        component(ArtList)
+            .prop('resource', resource)
+            .render()
+
+        expect(screen.getByText(posts.apple.title)).toBeInTheDocument()
+        expect(screen.getByText(posts.orange.title)).toBeInTheDocument()
     })
 })
--- src/client/art/ArtList/ArtList.svelte
+++ src/client/art/ArtList/ArtList.svelte
@@ -1,10 +1,32 @@
 <script lang="ts">
     import { DocumentInfo } from '@/client/DocumentInfo'
-    import { Container } from '@/client/Container'    
+    import { Container } from '@/client/Container'
+    import { Loading } from '@/client/Loading'
+    import { Error } from '@/client/Error'
+    import type { Resource, Maybe } from '@/client/resources'
+    import { Pending, Missing } from '@/client/resources'
+    import type { ArtItem } from '../types';
+
+    export let resource: Resource<ArtItem>
+
+    let items: Maybe<ArtItem[]> = Pending
+    $: items = resource.list()
 </script>
 
 <DocumentInfo title="Art" description="My name's Timothy! I code and teach for a living, and sometimes I write about programming, worldbuilding, and stuff I've built.">
     <Container>
-        <h1>Art</h1>
+        {#if items === Pending}
+            <Loading text="Fetching posts..." large />
+        {:else if items === Missing}
+            <Error title="Uh oh!" subtitle="Something went horribly wrong.">
+                <p>I recommend you try refreshing the page. If that doesn't fix it, I dunno, guess I need to fix something!</p>
+            </Error>
+        {:else}
+            <ul>
+                {#each items.filter(it => it.publishedAt) as item}
+                    <li>{item.title}</li>
+                {/each}
+            </ul>
+        {/if}
     </Container>
 </DocumentInfo>
