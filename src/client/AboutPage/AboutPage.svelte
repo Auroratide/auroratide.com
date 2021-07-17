2c50c83eb754ae4c30c2e6c3b9e2d19a7b47266a
Timothy Foster <tf.auroratide@gmail.com>
2021-07-17 08:45:39 -0500

    A decent use case for the hgroup tag

--- src/client/AboutPage/AboutPage.svelte
+++ src/client/AboutPage/AboutPage.svelte
@@ -10,19 +10,23 @@
 
 <DocumentInfo title="About" description="My name's Timothy! I code and teach for a living, and sometimes I write about programming, worldbuilding, and stuff I've built.">
     <Container>
-        <article class="page">
-            <Content>
-                <FocusOnMe><h1 class="greeting">Hi! I'm Timothy.</h1></FocusOnMe>
-                <h2 class="tagline">Coding is my craft. Teaching is my passion.</h2>
-                <img-colorscape class="profile" colorscape="{assetRoot.asset('profile/colorscape.svg')}">
-                    <img src={assetRoot.asset('profile/1080w.png')} alt="" />
-                </img-colorscape>
-                <p>A long time ago, I had a graphing calculator which came with a bunch of games on it. But one day, a nefarious colleague deleted all those games. Desperate, I sought a manual hoping they could be restored, but I stumbled upon something much better.</p>
-                <p>The games could not be undeleted. Instead, they could be recreated.</p>
-                <p>That's how I discovered programming. Since then, I've dabbled in websites, game development, artificial intelligence, and more. It turns out coding is much like magic.</p>
-                <p>Nowadays, I work as an independent developer. I deal mostly with Javascript code (fun stuff like Svelte, React, or Pixi). So if you're a frontend developer or perhaps a game designer, hopefully you will find the content on my site enriching!</p>
-            </Content>
-        </article>
+        <FocusOnMe>
+            <article class="page">
+                <Content>
+                    <hgroup>
+                        <h1 class="greeting">Hi! I'm Timothy.</h1>
+                        <h2 class="tagline">Coding is my craft. Teaching is my passion.</h2>
+                    </hgroup>
+                    <img-colorscape class="profile" colorscape="{assetRoot.asset('profile/colorscape.svg')}">
+                        <img src={assetRoot.asset('profile/1080w.png')} alt="" />
+                    </img-colorscape>
+                    <p>A long time ago, I had a graphing calculator which came with a bunch of games on it. But one day, a nefarious colleague deleted all those games. Desperate, I sought a manual hoping they could be restored, but I stumbled upon something much better.</p>
+                    <p>The games could not be undeleted. Instead, they could be recreated.</p>
+                    <p>That's how I discovered programming. Since then, I've dabbled in websites, game development, artificial intelligence, and more. It turns out coding is much like magic.</p>
+                    <p>Nowadays, I work as an independent developer. I deal mostly with Javascript code (fun stuff like Svelte, React, or Pixi). So if you're a frontend developer or perhaps a game designer, hopefully you will find the content on my site enriching!</p>
+                </Content>
+            </article>
+        </FocusOnMe>
     </Container>
 </DocumentInfo>
 
