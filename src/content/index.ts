5f5c800806412e0bd1e9b4540994f9a34e710bba
Timothy Foster <tf.auroratide@gmail.com>
2021-07-20 16:21:05 -0500

    [#276] Ability to compile content for single pages

--- src/content/index.ts
+++ src/content/index.ts
@@ -1,5 +1,6 @@
 import path from 'path'
 import { resource } from './resource'
+import { single } from './single'
 import { time } from './time'
 
 time('posts', () => resource({
@@ -18,4 +19,10 @@ time('art', () => resource({
     name: 'art',
     contentPath: path.resolve('content', 'art'),
     outputPath: path.resolve('public', 'api', 'art'),
+}))
+
+time('sandbox', () => single({
+    name: 'single',
+    contentPath: path.resolve('content', 'sandbox'),
+    outputPath: path.resolve('public', 'api', 'sandbox'),
 }))
---
+++ src/content/single/single.ts
@@ -0,0 +1,22 @@
+import path from 'path'
+import fs from 'fs/promises'
+import mkdirp from 'mkdirp'
+import marked from 'marked'
+
+interface Options {
+    name: string
+    contentPath: string
+    outputPath: string
+}
+
+const parse = async (dir: string) => {
+    const content = marked(await fs.readFile(path.join(dir, 'content.md'), { encoding: 'utf-8' }))
+
+    return { content }
+}
+
+export const single = async (options: Options) => {
+    await mkdirp(options.outputPath)
+
+    await fs.writeFile(path.join(options.outputPath, 'index.json'), JSON.stringify(await parse(options.contentPath)), { encoding: 'utf-8' })
+}
