7776e970d17a3c1f2f99a21a5883a9cb06af2604
Timothy Foster <tf.auroratide@gmail.com>
2021-08-28 15:29:53 -0500

    Modify skip link to be a bit prettier and more obvious

--- src/client/layout/SkipLink/SkipLink.svelte
+++ src/client/layout/SkipLink/SkipLink.svelte
@@ -11,10 +11,25 @@
         text-align: center;
         position: absolute;
         left: -9999px;
+        font-size: var(--sizing-font-md);
     }
 
     .skip-link:focus {
+        position: fixed;
+        top: var(--sizing-spacing-md);
+        left: var(--sizing-spacing-md);
+        z-index: 999;
+    }
+
+    .skip-link:focus::after {
+        content: '';
+        display: block;
+        position: fixed;
         top: 0;
+        right: 0;
+        bottom: 0;
         left: 0;
+        background-color: var(--palette-shade-033);
+        z-index: -2;
     }
 </style>
