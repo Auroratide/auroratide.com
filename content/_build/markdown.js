e076423ed2aac2037e11517858935046f83648e6
Timothy Foster <tf.auroratide@gmail.com>
2022-05-12 12:53:30 -0500

    Do not include non-alphanumeric symbols in id

--- content/_build/markdown.js
+++ content/_build/markdown.js
@@ -2,7 +2,9 @@ import { marked } from 'marked'
 
 const renderer = {
     heading(text, level) {
-        const escapedText = text.toLowerCase().replace(/\s+/g, '-')
+        const escapedText = text.toLowerCase()
+            .replace(/\s+/g, '-')
+            .replace(/[^a-z0-9\-]/g, '')
 
         return `<h${level} id="${escapedText}" class="anchored-heading">${text}<a href="#${escapedText}" class="heading-anchor"><vector-icon icon="link"></vector-icon><span class="visually-hidden">Anchor for ${text}</span></a></h${level}>`
     }
