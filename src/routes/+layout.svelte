ad74dd4920620f977b4e2549ad967f282ff85a45
Timothy Foster <tf.auroratide@gmail.com>
2024-06-03 07:11:27 -0500

    [redesign] Begin with fonts and colors

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,23 +1,25 @@
 <script lang="ts">
-    import Page from '$lib/layout/Page.svelte'
-    import Navigation from '$lib/layout/Navigation.svelte'
-    import Footer from '$lib/layout/Footer.svelte'
+	import Page from '$lib/layout/Page.svelte'
+	import Navigation from '$lib/layout/Navigation.svelte'
+	import Footer from '$lib/layout/Footer.svelte'
+
+	import "normalize.css"
 </script>
 
 <Page>
-    <div slot="header"><Navigation /></div>
-    <slot></slot>
-    <div slot="footer"><Footer /></div>
+	<div slot="header"><Navigation /></div>
+	<slot></slot>
+	<div slot="footer"><Footer /></div>
 </Page>
 
 <style>
-    @media print {
-        :global(body) {
-            background-color: white;
-        }
+	@media print {
+		:global(body) {
+			background-color: white;
+		}
 
-        [slot="header"], [slot="footer"] {
-            display: none;
-        }
-    }
+		[slot="header"], [slot="footer"] {
+			display: none;
+		}
+	}
 </style>
