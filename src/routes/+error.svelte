638bef5b0431ee6bef8fabb6fae501155f785bd6
Timothy Foster <tf.auroratide@gmail.com>
2024-06-28 08:15:22 -0500

    [redesign] Apply layout to error page

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,24 +1,31 @@
 <script lang="ts">
 	import { page } from '$app/stores'
-	import { Routes } from '$lib/auroratide/routes'
+	import { Logo } from '$lib/auroratide/Logo'
+	import { FooterNav, MainNav, Routes, Socials } from '$lib/auroratide/routes'
 	import { Button } from '$lib/design-system/Button'
+	import { PageLayout } from '$lib/design-system/PageLayout'
 	import { ErrorPage } from '$lib/design-system/pages'
+	import { SocialLinks } from '$lib/design-system/SocialLinks'
 
 	let title: string = ""
 	let message: string = ""
 
 	if ($page.status === 404) {
 		title = "Seems like there's nothing here."
-		message = "The page you're looking for is not available. Try using the top navigation bar to find cool, awesome content! Or, click the button below to go back to the home page."
+		message = "The page you're looking for is not available. Click the button below to go back to the home page."
 	} else {
 		title = "Something went horribly wrong."
-		message = "I recommend you try refreshing the page. If that doesn't fix it, I dunno, guess I need to fix something!"
+		message = "I recommend you try refreshing the page. If that doesn't fix it, then I probably mucked something up!"
 	}
 </script>
 
-<ErrorPage title="Uh oh!" subtitle={title}>
-	<svelte:fragment slot="content">
-		<p>{message}</p>
-		<p><Button href="{Routes.Home.href()}">Return Home</Button></p>
-	</svelte:fragment>
-</ErrorPage>
+<PageLayout headerNav={MainNav} footerNav={FooterNav}>
+	<Logo slot="logo" />
+	<ErrorPage title="Uh oh!" subtitle={title}>
+		<svelte:fragment slot="content">
+			<p>{message}</p>
+			<p><Button href="{Routes.Home.href()}">Return Home</Button></p>
+		</svelte:fragment>
+	</ErrorPage>
+	<SocialLinks slot="footer" title="Find me on" values={Object.values(Socials)} />
+</PageLayout>
