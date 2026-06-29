5436ddbf8350d8c035f33335cb776f5e9231de74
Auroratide <tf.auroratide@gmail.com>
2026-06-29 06:58:34 -0500

    Create a volunteering section to be optionally used

---
+++ src/routes/(standard-layout)/resume/Volunteering.svelte
@@ -0,0 +1,28 @@
+<section class="volunteering">
+	<h2>Volunteering</h2>
+	<p class="side-by-side"><strong>Bold Idea</strong><span>Jun 2026 - Present</span></p>
+	<p>Coding mentor, guiding elementary-school students through introductory programming projects, translating technical concepts for a non-technical audience.</p>
+</section>
+
+<style>
+	.volunteering {
+		grid-area: fun-facts;
+	}
+
+	p {
+		font-size: 0.8em;
+		line-height: 1.25;
+	}
+
+	.side-by-side {
+		display: flex;
+		justify-content: space-between;
+	}
+
+	@media print {
+		.volunteering {
+			background: var(--c-light-mid);
+			padding: 0 1.5em 1.5em;
+		}
+	}
+</style>
